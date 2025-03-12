a5f708a6fe25d2fb87d5e9cd8bdc3519b0a6207a
Sandhosh <sandhoshtechie02@gmail.com>
2025-03-12 19:48:35 +0530

    about

--- resources/navbar.php
+++ resources/navbar.php
@@ -38,7 +38,38 @@
 <div class="navbar" id="navbar">
 <ul>
 <ul>
-    <li><a href="#">About</a></li>
+    
+<li>
+
+    <div class="dropdown">
+    <button class="btn custom_dropdown " type="button" id="dropdownMenuButton"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        About
+                    </button>
+                    <div class="dropdown-menu custome-dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item" href="#">About Us</a>
+                        <a class="dropdown-item" href="#">Principal</a>
+                        <a class="dropdown-item" href="#">Mous</a>
+                        <a class="dropdown-item" href="#">Organisation</a>
+                        <a class="dropdown-item" href="#">Committees</a>
+                        <a class="dropdown-item" href="#">Annual Reports</a>
+                        <a class="dropdown-item" href="#">Annual Accounds</a>
+                        <a class="dropdown-item" href="#">Sponsoring Body</a>
+                        <a class="dropdown-item" href="#">Teaching Faculty</a>
+                        <a class="dropdown-item" href="#">Non-Teaching Faculty</a>
+
+
+
+
+
+
+
+
+                    </div>
+    </div>
+
+</li>
+
     <li>
 
     <div class="dropdown">
@@ -66,17 +97,102 @@
 
 </li>
     <li><a href="#">COE</a></li>
-    <li><a href="#">Academics</a></li>
-    <li><a href="#">Admissions</a></li>
-    <li><a href="#">Facilities</a></li>
-    <li><a href="#">Student Life</a></li>
+
+    <li>
+
+    <div class="dropdown">
+    <button class="btn custom_dropdown " type="button" id="dropdownMenuButton"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Academics
+                    </button>
+                    <div class="dropdown-menu custome-dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item" href="#">Regulations</a>
+                        <a class="dropdown-item" href="#">Curriculum and Syllabus</a>
+                        <a class="dropdown-item" href="#">Academic Schedule</a>
+                    </div>
+    </div>
+
+</li>
+
+<li>
+
+<div class="dropdown">
+<button class="btn custom_dropdown " type="button" id="dropdownMenuButton"
+                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Admissions
+                </button>
+                <div class="dropdown-menu custome-dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="#">Programmes Offered</a>
+                    <a class="dropdown-item" href="#">Eligibility</a>
+                    <a class="dropdown-item" href="#">Enquiry Form</a>
+                </div>
+</div>
+
+</li>
+
+
+    
+<li>
+
+<div class="dropdown">
+<button class="btn custom_dropdown " type="button" id="dropdownMenuButton"
+                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Facilities
+                </button>
+                <div class="dropdown-menu custome-dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="#">Administration Services</a>
+                    <a class="dropdown-item" href="#">Library</a>
+                    <!-- <a class="dropdown-item" href="#">Canteen</a> -->
+                    <!-- <a class="dropdown-item" href="#">Hostel</a> -->
+                    <a class="dropdown-item" href="#">Transport</a>
+                    <!-- <a class="dropdown-item" href="#">IT Infrastructure</a> -->
+                    <!-- <a class="dropdown-item" href="#">Health Centre</a> -->
+                    <!-- <a class="dropdown-item" href="#">Security</a> -->
+                    <!-- <a class="dropdown-item" href="#">Lecture Capturing System</a> -->
+                    <!-- <a class="dropdown-item" href="#">Green Campus</a>
+                    <a class="dropdown-item" href="#">Energy Conservation</a>
+                    <a class="dropdown-item" href="#">Water Conservation</a> -->
+                    <a class="dropdown-item" href="#">Infrastructure</a>
+                    <!-- <a class="dropdown-item" href="#">Maintenance</a> -->
+
+                </div>
+</div>
+
+</li>
+
+
+<li>
+
+<div class="dropdown">
+<button class="btn custom_dropdown " type="button" id="dropdownMenuButton"
+                    data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Student Life
+                </button>
+                <div class="dropdown-menu custome-dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="#">Sports Facilities</a>
+                    <a class="dropdown-item" href="#">Nss</a>
+                    <a class="dropdown-item" href="#">Hostel</a>
+                    <a class="dropdown-item" href="#">Placement Cell Activities</a>
+                    <a class="dropdown-item" href="#">SGRC</a>
+                    <a class="dropdown-item" href="#">Health Facilities</a>
+                    <a class="dropdown-item" href="#">ICC</a>
+                    <a class="dropdown-item" href="#">Anti-Ragging Cell</a>
+                    <a class="dropdown-item" href="#">EO cell</a>
+                    <a class="dropdown-item"  href="#">SEDG</a>
+                    <a class="dropdown-item" href="#"> Specially-Abled Support </a>
+
+                    
+                    
+
+                </div>
+</div>
+
+</li>
+    
     <li><a href="#">Placements</a></li>
     <li><a href="#">Research</a></li>
-    <li><a href="#">IIC</a></li>
-    <li><a href="#">IIIC</a></li>
+    <li><a href="#">Administration</a></li>
     <li><a href="#">Alumni</a></li>
-    <li><a href="#">IR</a></li>
-    <li><a href="#">IQAC</a></li>
     <li><a href="#">NAAC</a></li>
 </ul>
 
