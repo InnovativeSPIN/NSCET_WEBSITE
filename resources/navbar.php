d9de6566a3805af0e4bb31d96bf6e2560e5da632
Sandhosh <sandhoshtechie02@gmail.com>
2025-03-13 20:23:12 +0530

    Nav

--- resources/navbar.php
+++ resources/navbar.php
@@ -60,6 +60,193 @@
 
             <?php include("navlinks.php") ?>
 
+<<<<<<< Updated upstream
+=======
+                <div class="dropdown">
+                    <button class="btn custom_dropdown " type="button" id="dropdownMenuButton"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        About
+                    </button>
+                    <div class="dropdown-menu custome-dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item" href="/about/index.php">About Us</a>
+                        <a class="dropdown-item" href="/principal/index.php">Principal</a>
+                        <a class="dropdown-item" href="/gallery">Gallery</a>
+                        <a class="dropdown-item" href="#">Mou's</a>
+                        <!-- <a class="dropdown-item" href="#">Organisation</a>
+                        <a class="dropdown-item" href="#">Committees</a> -->
+                        <a class="dropdown-item" href="/assets/pdf/ANNUAL REPORT.pdf">Annual Reports</a>
+                        <a class="dropdown-item" href="/assets/pdf/ANNUAL ACCOUNTS.pdf">Annual Accounds</a>
+                    </div>
+                </div>
+
+            </li>
+            <li><a href="#">Administration</a></li>
+            <li>
+
+                <div class="dropdown">
+                    <button class="btn custom_dropdown " type="button" id="dropdownMenuButton"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Department
+                    </button>
+                    <div class="dropdown-menu custome-dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item" href="/department/?dept=cse">Computer Science &amp;
+                            Engineering</a>
+                        <a class="dropdown-item" href="/department/?dept=civil">Civil Engineering</a>
+                        <a class="dropdown-item" href="/department/?dept=ece">Electronics &amp;
+                            Communication Engineering</a>
+                        <a class="dropdown-item" href="/department/?dept=eee">Electrical and Electronics
+                            Engineering</a>
+                        <a class="dropdown-item" href="/department/?dept=mech">Mechanical Engineering</a>
+                        <a class="dropdown-item" href="/department/?dept=ai-and-ds">Artificial Intelligence
+                            &amp; Data Science </a>
+                        <a class="dropdown-item" href="/department/?dept=it">Information Technology</a>
+                        <a class="dropdown-item" href="/department/?dept=s-and-h">Science and Humanities</a>
+                        <a class="dropdown-item" href="/department/?dept=se">Structural Engineering</a>
+                        <a class="dropdown-item" href="/department/?dept=mfe">Manufacturing Engineering</a>
+                    </div>
+                </div>
+
+            </li>
+            <li>
+
+<div class="dropdown">
+    <button class="btn custom_dropdown " type="button" id="dropdownMenuButton"
+        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Conference
+    </button>
+    <div class="dropdown-menu custome-dropdown-menu" aria-labelledby="dropdownMenuButton">
+        <a class="dropdown-item" href="/admission/index.php">ICRTT 25</a>
+        <a class="dropdown-item" href="#">AMPSE25</a>
+        <a class="dropdown-item" href="#">ICAMS 2K25</a>
+        
+
+
+
+    </div>
+</div>
+
+</li>
+            
+
+            <li>
+
+                <div class="dropdown">
+                    <button class="btn custom_dropdown " type="button" id="dropdownMenuButton"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Academics
+                    </button>
+                    <div class="dropdown-menu custome-dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item" href="#">Details of Academic</a>
+                        <a class="dropdown-item" href="#">Academic Calender</a>
+                        <a class="dropdown-item" href="/teaching/index.php">Teaching Faculty</a>
+                        <a class="dropdown-item" href="/non-teaching/index.php">Non-Teaching Faculty</a>
+                        <a class="dropdown-item" href="#">UGc ODL online Programs</a>
+                        <a class="dropdown-item" href="/iqac/">IQAC</a>
+                        <a class="dropdown-item" href="#">Academic Collab</a>
+                        <a class="dropdown-item" href="/library/index.php">Library</a>
+
+
+                    </div>
+                </div>
+
+            </li>
+
+            <li>
+
+                <div class="dropdown">
+                    <button class="btn custom_dropdown " type="button" id="dropdownMenuButton"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Admissions & Fee
+                    </button>
+                    <div class="dropdown-menu custome-dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item" href="/admission/index.php">Admission details</a>
+                        <a class="dropdown-item" href="#">Prospectus</a>
+                        <a class="dropdown-item" href="#">Admission process</a>
+                        <a class="dropdown-item" href="#">Fee Refund Policy</a>
+
+
+
+                    </div>
+                </div>
+
+            </li>
+
+
+
+            <li>
+
+                <div class="dropdown">
+                    <button class="btn custom_dropdown " type="button" id="dropdownMenuButton"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Facilities
+                    </button>
+                    <div class="dropdown-menu custome-dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item" href="#">Administration Services</a>
+                        <a class="dropdown-item" href="#">Sports Facilities</a>
+                        <a class="dropdown-item" href="#">Health Facilities</a>
+                        <!-- <a class="dropdown-item" href="#">Library</a> -->
+                        <!-- <a class="dropdown-item" href="#">Canteen</a> -->
+                        <!-- <a class="dropdown-item" href="#">Hostel</a> -->
+                        <a class="dropdown-item" href="#">Transport</a>
+                        <!-- <a class="dropdown-item" href="#">IT Infrastructure</a> -->
+                        <!-- <a class="dropdown-item" href="#">Health Centre</a> -->
+                        <!-- <a class="dropdown-item" href="#">Security</a> -->
+                        <!-- <a class="dropdown-item" href="#">Lecture Capturing System</a> -->
+                        <!-- <a class="dropdown-item" href="#">Green Campus</a>
+                    <a class="dropdown-item" href="#">Energy Conservation</a>
+                    <a class="dropdown-item" href="#">Water Conservation</a> -->
+                        <a class="dropdown-item" href="#">Infrastructure</a>
+                        <!-- <a class="dropdown-item" href="#">Maintenance</a> -->
+
+                    </div>
+                </div>
+
+            </li>
+
+
+            <li>
+
+                <div class="dropdown">
+                    <button class="btn custom_dropdown " type="button" id="dropdownMenuButton"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Student Life
+                    </button>
+                    <div class="dropdown-menu custome-dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="/sports/">Sports</a>
+                        <a class="dropdown-item" href="#">Nss</a>
+                        <a class="dropdown-item" href="#">Hostel</a>
+                        <a class="dropdown-item" href="#">Placement Cell Activities</a>
+                        <a class="dropdown-item" href="#">SGRC</a>
+                        <a class="dropdown-item" href="#">ICC</a>
+                        <a class="dropdown-item" href="#">Anti-Ragging Cell</a>
+                        <a class="dropdown-item" href="#">EO cell</a>
+                        <a class="dropdown-item" href="#">SEDG</a>
+                        <a class="dropdown-item" href="#"> Specially-Abled Support </a>
+
+
+
+
+                    </div>
+                </div>
+
+            </li>
+
+            <li><a href="/placement/">Placements</a></li>
+
+
+            <li>
+                <div class="dropdown">
+                    <button class="btn custom_dropdown " type="button" id="dropdownMenuButton"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        More
+                    </button>
+                    <div class="dropdown-menu custome-dropdown-menu naac_dropdown" aria-labelledby="dropdownMenuButton">
+                        <a href="/research/" class="dropdown-item">Research</a>
+                        <a href="#" class="dropdown-item">Alumni</a>
+                    </div>
+                </div>
+            </li>
+>>>>>>> Stashed changes
 
         </ul>
 
