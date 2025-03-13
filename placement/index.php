02b13730a02bb4f132fa17b62e98b29f390e40a0
bnaveenbharathi <naveenbharathi5050@gmail.com>
2025-03-13 12:49:12 +0530

    placement page updated

---
+++ placement/index.php
@@ -0,0 +1,262 @@
+<?php 
+
+include('../resources/conn.php');
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Placements | <?php echo $colle_Name; ?></title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
+
+<!-- custom css -->
+<link rel="stylesheet" href="../assets/css/resources/style.css">
+<link rel="stylesheet" href="../assets/css/placement/style.css">
+
+<!-- custom js -->
+<script src="https://kit.fontawesome.com/181ea7bd20.js" crossorigin="anonymous"></script>
+</head>
+<body>
+
+<?php include('../resources/navbar.php');?>
+
+
+
+
+<section class="hero-section" style="background: url('../assets/img/main/college_main.jpg') no-repeat center/cover;">
+  <h1 class="dept-heading">Career Placement</h1>
+</section>
+<div class="container mt-5">
+<main >
+
+  <h2 class="about">About Placement</h2>
+  <p class="about_text">The Training & Placement Department of NSCET has been in the forefront of activities, bringing corporate close to the campus and encouraging them to establish facilities to cater to the needs of the students. It plays a pivotal role in counseling and guiding the students for their successful career, which is a crucial interface between the stages of completion of academic program of the students and their entry into the job market. Its aim is to provide career guidance and placement opportunities to all students studying in NSCET. Training and Placement Department arranges and coordinates various programs that aim at moulding the students so as to meet the industry expectations in their career building and in turn bring laurels to the parent institution. The department sincerely believes in imparting comprehensive training to the students to face the challenges of the dynamic and demanding job market and build a successful career for themselves. Special SKILL ENHANCEMENT TRAINING PROGRAMS have been customized for students so as to meet the industry requirement..</p>
+
+</main>
+</div>
+
+<div class="placement_img">
+  
+<div class="main_coor">
+  <div class="coordinator">
+    <h2 class="about">Coordinator</h2>
+    <img class="coordinator_img" src="../assets/img/placement/dr_karthikeyan.png" alt="Mr. Karthikeyan">
+    <p class="text-center">DR.C.Karthikeyan <br>Training and Placement Officer</p>
+  </div>
+
+  <div class="roles ">
+    <h4 class="main_coor_h4">Roles and Responsibilities</h4>
+    <p class="main_coor_p">The Training and Placement Department, guided by a set of rules and principles, strives to maintain a good relationship with industries. The department endeavors to successfully carry out all the processes related to training, placement, and career guidance methodically throughout the year.</p>
+  </div>
+</div>
+</div>
+<!-- 
+<div class="hightlight">
+  <h2 class="hight_h2">Placement Highlights</h2> -->
+  <!-- <div class="back_img-container">
+    <div class="back_img">
+      <img class="high_img" src="../assets/img/placement/zoho-logo-web.svg" alt="">
+    </div>
+    <div class="back_img">
+      <img class="high_img" src="../assets/img/placement/tata-consultancy-services-1.svg" alt="">
+    </div>
+    <div class="back_img">
+      <img class="high_img" src="../assets/img/placement/tata-consultancy-services-1.svg" alt="">
+    </div>
+    <div class="back_img">
+      <img class="high_img" src="../assets/img/placement/tata-consultancy-services-1.svg" alt="">
+    </div>
+    <div class="back_img">
+      <img class="high_img" src="../assets/img/placement/tata-consultancy-services-1.svg" alt="">
+    </div> -->
+  
+    
+  </div>
+  
+<!--   
+  <div class="placement-stats">
+
+  <div class="stat-item">
+      <span class="stat-number">89% <br> <spam class="placement_spam"> Placement </spam></span>
+      
+    </div>
+    <div class="stat-item">
+      <span class="stat-number">827 <br> <spam class="placement_spam"> Students placed </spam></span>
+      
+    </div>
+    <div class="stat-item">
+      <span class="stat-number">19LPA% <br> <spam class="placement_spam"> Highest salary package </spam></span>
+      
+    </div>
+    <div class="stat-item">
+      <span class="stat-number">165 <br> <spam class="placement_spam"> Recruiters </spam></span>
+      
+    </div>
+
+
+  </div>
+  
+</div> -->
+
+<section class="facilities mt-5 mb-5">
+        <div class="container">
+            <h2>Facilities Provided to Recruiters</h2>
+            <p class="about_text mt-4 mb-5">A state of the art air conditioned auditorium with a seating capacity of over 400 students
+                An air conditioned, multimedia equipped Interaction Hall with seating capacity of around 60 students for interacting with the students and conducting workshops/Seminars
+                Fully equipped, air conditioned computer labs for conducting online test of around 300 students at a time.
+                A multimedia air conditioned conference hall for conducting Group Discussions.
+                Well furnished and fully equipped cabins for conducting personal Interviews. High Speed Wi-Fi Internet.</p>
+            <div class="facility-list">
+                <div class="facility">
+                    <h3>Auditorium</h3>
+                    <p>State-of-the-art air-conditioned auditorium with a capacity of over 400 students.</p>
+                </div>
+                <div class="facility">
+                    <h3>Interaction Hall</h3>
+                    <p>Air-conditioned, multimedia-equipped hall for interactions and workshops.</p>
+                </div>
+                <div class="facility">
+                    <h3>Computer Labs</h3>
+                    <p>Fully equipped labs for conducting online tests for up to 300 students at once.</p>
+                </div>
+                <div class="facility">
+                    <h3>Conference Hall</h3>
+                    <p>Multimedia air-conditioned hall for conducting group discussions.</p>
+                </div>
+                <div class="facility">
+                    <h3>Personal Interviews</h3>
+                    <p>Fully equipped cabins for conducting personal interviews with high-speed Wi-Fi internet.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="corporate-training">
+        <div class="container">
+            <h2>Corporate Training</h2>
+            <p class="about_text mt-4">Students are given aptitude, soft skill,communication training on a perennial basis. This sharpens their employability skills and Confidence in situation handling during recruitment drives. Training in Group Discussions, Mock Interview sessions are periodically done so as to update the students and to make them realise where they stand in the arena of employment.</p>
+        </div>
+    </section>
+    <section class=" mt-5 Communication Skill Enhancement Training">
+        <div class="container">
+            <h2>Communication Skill Enhancement Training</h2>
+            <p class="about_text mt-4">Focusing to improve their vocabulary so that they can express their views without any hesitation in all situation.
+                How to perform oneself during interview.
+                How to exhibit attitude, skills & knowledge during interview.
+                How to present papers and seminars.
+                Competitive & Written examinations for recruitment
+                Management Reviews
+                Research Papers
+                Updates from newspapers & world class magazines
+                General & Business quiz
+                Latest company updates</p>
+            <h2 class="mt-4 mb-4">Activities in CSET</h2> 
+            <p class="about_text">Verbal Training
+                Soft Skills Training
+                Discussions on Contemporary Issues
+                Senior Interaction on Summer Internship and Final Placements
+                Newspaper Reading and Discussion
+                Research Papers
+                Article Reading and Discussion
+                Industrial Visits and Interactions
+                Interactions with Alumni on Placement and Summer Internships</p>   
+        </div>
+    </section>
+  
+    <section class = 'partnership-section'>
+        <div class = 'container'>
+            <h2>Industry Institute Partnership Cell</h2>
+            <p class="about_text mt-4">The Industry Institute Partnership Cell is established to bridge the skill gap between institute and industry. The key challenge faced by the engineering education today is the need to constantly adapt to the changing requirements of industry.
+
+                The IIPC identifies the industrial expectation and promotes institutional preparation for meeting industrial needs by facilitating R&D; projects, seminars, workshops and various other industrial training programs. It also implies in equipping faculty to latest technologies and makes the students industry-ready by providing exposure to current industry practices, and sharpens their skills to adapt changing technologies.
+                
+                The crucial focus of IIPC is to interact with elite industries in India and extend the efforts in establishing partnership with industries across the globe in near future.The objective of the IIP Cell is to facilitate the future technocrats to meet out the industry expectations through academic offerings with involvement of industry and accomplish a high contemporary technological environment.
+                
+                The IIPC disseminates technical advances through informal educational techniques which includes internships, industrial visits, participating industrial events etc.</p>
+        </div>
+    </section>
+
+    
+ <div class="student_image">
+ <h2 class="req_h2 placed ">2024-25 Placed Students </h2>
+ <div class="card_sliderr">
+<div class="d-flex justify-content-center mt-5">
+<img src="../assets/img/placement/placement25.png" alt="" width="1280px">
+</div>
+
+</div>
+ </div>
+
+<!-- 
+ <div class="req">
+<h2 class="req_h2">Our Recruiters </h2>
+
+<marquee scrollamount="15" >
+ 
+  <div class="logos">
+
+  <img class="mar_img" src="../assets/img/placement/zoho-logo-web.svg" alt="">
+  <img class="mar_img" src="../assets/img/placement/zoho-logo-web.svg" alt="">
+  <img class="mar_img" src="../assets/img/placement/zoho-logo-web.svg" alt="">
+  <img class="mar_img" src="../assets/img/placement/zoho-logo-web.svg" alt="">
+  <img class="mar_img" src="../assets/img/placement/zoho-logo-web.svg" alt="">
+  <img class="mar_img" src="../assets/img/placement/zoho-logo-web.svg" alt="">
+  <img class="mar_img" src="../assets/img/placement/zoho-logo-web.svg" alt="">
+  <img class="mar_img" src="../assets/img/placement/zoho-logo-web.svg" alt="">
+  <img class="mar_img" src="../assets/img/placement/zoho-logo-web.svg" alt="">
+  <img class="mar_img" src="../assets/img/placement/zoho-logo-web.svg" alt="">
+  <img class="mar_img" src="../assets/img/placement/zoho-logo-web.svg" alt="">
+  <img class="mar_img" src="../assets/img/placement/zoho-logo-web.svg" alt="">
+ 
+
+  </div>
+
+
+</marquee>
+
+</div> 
+  
+     -->
+
+
+<div class="placement_con">
+    <h2 class="con_h2">Contact Details</h2>
+    <div class="contact_card">
+        <p>Mr. Karthikeyan<br>
+        Training & Placement Officer<br>
+        Nadar Saraswathi College Of Engineering & Technology<br>
+        Vadapudhupatti, Theni-625531<br>
+        Tamil Nadu, India<br>
+        <strong>TEL:</strong> 04546 – 253900, 901, 902<br>
+        <strong>EMAIL:</strong> <a href="mailto:placement@nscet.org">placement@nscet.org</a></p>
+    </div>
+</div>
+
+
+<div class="placement-activities">
+  <h2 class="con_h2">
+    Placement Activities
+  </h2>
+
+  <div class="placement_acitivites_main">
+
+  </div>
+</div>
+
+
+
+
+
+<?php include('../resources/footer.php'); ?>
+
+<!-- custom js -->
+<script src="./assets/js/resources/script.js"></script>
+</body>
+</html>
