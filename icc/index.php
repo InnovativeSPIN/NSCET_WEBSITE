47262c7ca4428c4e1c5890b57d3e9ffbfcf5a176
Sandhosh <sandhoshtechie02@gmail.com>
2025-03-20 21:43:23 +0530

    icc page update

--- icc/index.php
+++ icc/index.php
@@ -7,7 +7,7 @@ include('../resources/conn.php');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home | <?php echo $colle_Name; ?></title>
+    <title>ICC | <?php echo $colle_Name; ?></title>
     
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" 
