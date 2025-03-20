3a3e2213d65a364f559adc897a3709a2f59012f4
Sandhosh <sandhoshtechie02@gmail.com>
2025-03-20 21:09:51 +0530

    idp responsive

--- Idp/index.php
+++ Idp/index.php
@@ -1,10 +1,6 @@
 <?php
-
 include('../resources/conn.php');
-
-
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -16,54 +12,61 @@ include('../resources/conn.php');
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
-
-  <!-- custom css -->
   <link rel="stylesheet" href="../assets/css/resources/style.css">
   <link rel="stylesheet" href="../assets/css/non-teaching/style.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
-
-
-
-  <!-- custom js -->
   <script src="https://kit.fontawesome.com/181ea7bd20.js" crossorigin="anonymous"></script>
+  <style>
+    .main {
+      padding: 20px;
+    }
+    h1 {
+      margin-bottom: 30px;
+      margin-top: 30px;
+      font-size: 36px;
+      color: #333;
+      text-align: center;
+    }
+    img {
+      width: 95%;
+      height: auto;
+      object-fit: cover;
+      margin-left: auto;
+      margin-right: auto;
+      display: block;
+    }
+    @media (max-width: 768px) {
+      h1 {
+        font-size: 24px;
+        
+        margin-top: 80px;
+      }
+      img {
+        width: 100%;
+        height: auto;
+        margin-left: 0;
+      }
+    }
+  </style>
 </head>
 
 <body>
 
   <?php
-
   include('../resources/navbar.php');
-
-
   ?>
 
-
-<div class="main">
-
-<h1 style=" margin-bottom:30px; margin-top:30px;  font-size: 36px; color: #333; text-align: center;">Institutional Development Plan</h1>
-
-<img src="/assets/img/idp/idp.jpg" alt="idp" style=" margin-left:40px; width: 95%; height: 100vh; object-fit: cover;">
-
-</div>
-
-
-
+  <div class="main">
+    <h1>Institutional Development Plan</h1>
+    <img src="/assets/img/idp/idp.jpg" alt="idp">
+  </div>
 
   <?php
-
   include('../resources/footer.php');
-
-
   ?>
 
-
-
-
-  <!-- custom js -->
   <script src="../assets/js/resources/script.js"></script>
-  <!-- swiper -->
   <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-
 </body>
 
 </html>
