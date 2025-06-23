0dff572c11ae27c125ceaca3f4bf9477dfac9463
B.Naveen Bharathi <144258519+bnaveenbharathi@users.noreply.github.com>
2025-06-23 14:38:43 +0530

    nav update

--- resources/navlinks.php
+++ resources/navlinks.php
@@ -6,17 +6,17 @@
             Home
         </button>
         <div class="dropdown-menu custome-dropdown-menu" aria-labelledby="dropdownMenuButton">
-            <a class="dropdown-item" href="/about/">About us</a>
-            <a class="dropdown-item" href="/policy">Quality Policy</a>
-            <!--<a class="dropdown-item" href="/principal/">Principal</a>-->
+            <a class="dropdown-item" href="./about/">About us</a>
+            <a class="dropdown-item" href="./policy">Quality Policy</a>
+            <!--<a class="dropdown-item" href="./principal/">Principal</a>-->
             
             
             
             <!-- <a class="dropdown-item" href="#">Organisation</a> -->
-            <a class="dropdown-item" href="/Idp/">IDP</a>
-            <a class="dropdown-item" href="/aff_accr">Affiliation & Accrediation</a>
-            <a class="dropdown-item" href="/files/AR.pdf">Annual Reports</a>
-            <a class="dropdown-item" href="/files/AC.pdf">Annual Accounts</a>
+            <a class="dropdown-item" href="./Idp/">IDP</a>
+            <a class="dropdown-item" href="./aff_accr">Affiliation & Accrediation</a>
+            <a class="dropdown-item" href="./files/AR.pdf">Annual Reports</a>
+            <a class="dropdown-item" href="./files/AC.pdf">Annual Accounts</a>
         </div>
     </div>
 
@@ -28,10 +28,10 @@
             Administration
         </button>
         <div class="dropdown-menu custome-dropdown-menu" aria-labelledby="dropdownMenuButton">
-            <a class="dropdown-item" href="/administration/">TMHNU Trust</a>
-            <a class="dropdown-item" href="/principal/">Principal</a>
-            <a class="dropdown-item" href="/control_of_exam">Controller of Examination</a>
-            <a class="dropdown-item" href="/files/ICC-NSCET.pdf">ICC</a>
+            <a class="dropdown-item" href="./administration/">TMHNU Trust</a>
+            <a class="dropdown-item" href="./principal/">Principal</a>
+            <a class="dropdown-item" href="./control_of_exam">Controller of Examination</a>
+            <a class="dropdown-item" href="./files/ICC-NSCET.pdf">ICC</a>
         </div>
     </div>
 
@@ -45,20 +45,20 @@
             Department
         </button>
         <div class="dropdown-menu custome-dropdown-menu" aria-labelledby="dropdownMenuButton">
-            <a class="dropdown-item" href="/department/?dept=cse">Computer Science &amp;
+            <a class="dropdown-item" href="./department/?dept=cse">Computer Science &amp;
                 Engineering</a>
-            <a class="dropdown-item" href="/department/?dept=civil">Civil Engineering</a>
-            <a class="dropdown-item" href="/department/?dept=ece">Electronics &amp;
+            <a class="dropdown-item" href="./department/?dept=civil">Civil Engineering</a>
+            <a class="dropdown-item" href="./department/?dept=ece">Electronics &amp;
                 Communication Engineering</a>
-            <a class="dropdown-item" href="/department/?dept=eee">Electrical and Electronics
+            <a class="dropdown-item" href="./department/?dept=eee">Electrical and Electronics
                 Engineering</a>
-            <a class="dropdown-item" href="/department/?dept=mech">Mechanical Engineering</a>
-            <a class="dropdown-item" href="/department/?dept=ai-and-ds">Artificial Intelligence
+            <a class="dropdown-item" href="./department/?dept=mech">Mechanical Engineering</a>
+            <a class="dropdown-item" href="./department/?dept=ai-and-ds">Artificial Intelligence
                 &amp; Data Science </a>
-            <a class="dropdown-item" href="/department/?dept=it">Information Technology</a>
-            <a class="dropdown-item" href="/department/?dept=s-and-h">Science and Humanities</a>
-            <a class="dropdown-item" href="/department/?dept=se">Structural Engineering</a>
-            <a class="dropdown-item" href="/department/?dept=mfe">Manufacturing Engineering</a>
+            <a class="dropdown-item" href="./department/?dept=it">Information Technology</a>
+            <a class="dropdown-item" href="./department/?dept=s-and-h">Science and Humanities</a>
+            <a class="dropdown-item" href="./department/?dept=se">Structural Engineering</a>
+            <a class="dropdown-item" href="./department/?dept=mfe">Manufacturing Engineering</a>
         </div>
     </div>
 
@@ -73,9 +73,9 @@
             Conference
         </button>
         <div class="dropdown-menu custome-dropdown-menu" aria-labelledby="dropdownMenuButton">
-            <a class="dropdown-item" href="/ICRTT2025/">ICRTT 25</a>
-            <a class="dropdown-item" href="/ampse25">AMPSE25 </a>
-            <a class="dropdown-item" href="/icams2k25/">ICAMS 2K25</a>
+            <a class="dropdown-item" href="./ICRTT2025/">ICRTT 25</a>
+            <a class="dropdown-item" href="./ampse25">AMPSE25 </a>
+            <a class="dropdown-item" href="./icams2k25/">ICAMS 2K25</a>
 
         </div>
     </div>
@@ -90,15 +90,15 @@
             Academics
         </button>
         <div class="dropdown-menu custome-dropdown-menu" aria-labelledby="dropdownMenuButton">
-            <a class="dropdown-item" href="/detail_acadamics">Details of Academic Programs</a>
-            <a class="dropdown-item" href="/academic_calendar/">Academic Calender</a>
-            <a class="dropdown-item" href="/teaching/">Teaching Faculty</a>
-            <a class="dropdown-item" href="/non-teaching/">Non-Teaching Faculty</a>
+            <a class="dropdown-item" href="./detail_acadamics">Details of Academic Programs</a>
+            <a class="dropdown-item" href="./academic_calendar/">Academic Calender</a>
+            <a class="dropdown-item" href="./teaching/">Teaching Faculty</a>
+            <a class="dropdown-item" href="./non-teaching/">Non-Teaching Faculty</a>
             <!--<a class="dropdown-item" href="#">UGc ODL online Programs</a>-->
-            <a class="dropdown-item" href="/iqac/">IQAC</a>
-            <a class="dropdown-item" href="/mou/">Academic Collaboration</a>
-            <!--<a class="dropdown-item" href="/mou/">Mou's</a>-->
-            <a class="dropdown-item" href="/library/">Library</a>
+            <a class="dropdown-item" href="./iqac/">IQAC</a>
+            <a class="dropdown-item" href="./mou/">Academic Collaboration</a>
+            <!--<a class="dropdown-item" href="./mou/">Mou's</a>-->
+            <a class="dropdown-item" href="./library/">Library</a>
 
 
         </div>
@@ -114,11 +114,11 @@
             Admissions & Fee
         </button>
         <div class="dropdown-menu custome-dropdown-menu" aria-labelledby="dropdownMenuButton">
-            <a class="dropdown-item" href="/admission/#overview">Admission details</a>
-            <a class="dropdown-item" href="/admission/#prospectus">Prospectus</a>
-            <a class="dropdown-item" href="/admission/#process">Admission process</a>
-            <a class="dropdown-item" href="/admission/#form">Enquiry Form</a>
-<a class="dropdown-item" href="/admission/refund_policy.php">Refund Policy</a>
+            <a class="dropdown-item" href="./admission/#overview">Admission details</a>
+            <a class="dropdown-item" href="./admission/#prospectus">Prospectus</a>
+            <a class="dropdown-item" href="./admission/#process">Admission process</a>
+            <a class="dropdown-item" href="./admission/#form">Enquiry Form</a>
+<a class="dropdown-item" href="./admission/refund_policy.php">Refund Policy</a>
 
         </div>
     </div>
@@ -134,13 +134,13 @@
 <!--            Facilities-->
 <!--        </button>-->
 <!--        <div class="dropdown-menu custome-dropdown-menu" aria-labelledby="dropdownMenuButton">-->
-<!--        <a class="dropdown-item" href="/infrastructure">Infrastructure</a>-->
+<!--        <a class="dropdown-item" href="./infrastructure">Infrastructure</a>-->
             <!--<a class="dropdown-item" href="#">Administration Services</a>-->
-<!--            <a class="dropdown-item" href="/sports_facilites/">Sports Facilities</a>-->
-<!--            <a class="dropdown-item" href="/health/">Health Facilities</a>-->
+<!--            <a class="dropdown-item" href="./sports_facilites/">Sports Facilities</a>-->
+<!--            <a class="dropdown-item" href="./health/">Health Facilities</a>-->
             <!-- <a class="dropdown-item" href="#">Canteen</a> -->
             <!-- <a class="dropdown-item" href="#">Hostel</a> -->
-<!--            <a class="dropdown-item" href="/transport/">Transport Facilities</a>-->
+<!--            <a class="dropdown-item" href="./transport/">Transport Facilities</a>-->
             <!-- <a class="dropdown-item" href="#">IT Infrastructure</a> -->
             <!-- <a class="dropdown-item" href="#">Health Centre</a> -->
             <!-- <a class="dropdown-item" href="#">Security</a> -->
@@ -165,22 +165,22 @@
             Student Life
         </button>
         <div class="dropdown-menu custome-dropdown-menu" aria-labelledby="dropdownMenuButton">
-        <a class="dropdown-item" href="/sports/">Sports</a>
-            <a class="dropdown-item" href="/nss/">Nss</a>
-            <a class="dropdown-item" href="/hostel/boys_hostel/">Boys Hostel</a>
-            <a class="dropdown-item" href="/hostel/girls_hostel/">Girls Hostel</a>
-            <a class="dropdown-item" href="/sgrc/">SGRC</a>
-            <a class="dropdown-item" href="/anti_ragging/">Anti-Ragging Cell</a>
+        <a class="dropdown-item" href="./sports/">Sports</a>
+            <a class="dropdown-item" href="./nss/">Nss</a>
+            <a class="dropdown-item" href="./hostel/boys_hostel/">Boys Hostel</a>
+            <a class="dropdown-item" href="./hostel/girls_hostel/">Girls Hostel</a>
+            <a class="dropdown-item" href="./sgrc/">SGRC</a>
+            <a class="dropdown-item" href="./anti_ragging/">Anti-Ragging Cell</a>
             <a class="dropdown-item" href="#">EO cell</a>
-            <a class="dropdown-item" href="/files/ICC-NSCET.pdf">ICC</a>
+            <a class="dropdown-item" href="./files/ICC-NSCET.pdf">ICC</a>
             <!-- <a class="dropdown-item" href="#">SEDG</a>
             <a class="dropdown-item" href="#"> Specially-Abled Support </a> -->
             
-            <a class="dropdown-item" href="/sports_facilites/">Sports Facilities</a>
-            <a class="dropdown-item" href="/health/">Health Facilities</a>
+            <a class="dropdown-item" href="./sports_facilites/">Sports Facilities</a>
+            <a class="dropdown-item" href="./health/">Health Facilities</a>
             <!-- <a class="dropdown-item" href="#">Canteen</a> -->
             <!-- <a class="dropdown-item" href="#">Hostel</a> -->
-            <a class="dropdown-item" href="/transport/">Transport Facilities</a>
+            <a class="dropdown-item" href="./transport/">Transport Facilities</a>
             
         </div>
     </div>
@@ -190,7 +190,7 @@
 <li>
     <button class="btn custom_dropdown " 
         aria-haspopup="true" aria-expanded="false">
-        <a href="/placement/">
+        <a href="./placement/">
         Placements
         </a>
     </button>
@@ -205,14 +205,14 @@
             More
         </button>
         <div class="dropdown-menu custome-dropdown-menu naac_dropdown" aria-labelledby="dropdownMenuButton">
-            <a href="/research/" class="dropdown-item">Research</a>
-            <a href="/files/edc_cell.pdf" class="dropdown-item">EDC</a>
-            <a href="/alumni/" class="dropdown-item">Alumni</a>
-            <a href="/clubs_and_chapters/" class="dropdown-item">Clubs & Chapters</a>
-            <a href="/NIRF/" class="dropdown-item">NIRF</a>
-            <a href="/assets/pdf/naac/RTI.pdf" class="dropdown-item">RTI</a>
-            <a href="/naac/ssr/index.php" class="dropdown-item">SSR  </a>
-            <a class="dropdown-item" href="/gallery/">Gallery</a>
+            <a href="./research/" class="dropdown-item">Research</a>
+            <a href="./files/edc_cell.pdf" class="dropdown-item">EDC</a>
+            <a href="./alumni/" class="dropdown-item">Alumni</a>
+            <a href="./clubs_and_chapters/" class="dropdown-item">Clubs & Chapters</a>
+            <a href="./NIRF/" class="dropdown-item">NIRF</a>
+            <a href="./assets/pdf/naac/RTI.pdf" class="dropdown-item">RTI</a>
+            <a href="./naac/ssr/index.php" class="dropdown-item">SSR  </a>
+            <a class="dropdown-item" href="./gallery/">Gallery</a>
         </div>
     </div>
 </li>
